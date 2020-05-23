bdf9dc771a0fc07e1a407908c800581c7ac4d5f8
Annie Huang <annie.hn@gmail.com>
2020-05-23 21:29:40 +1000

    Created the cypress test first, which will cause "nx e2e shop-e2e --watch" run to fail

--- apps/shop-e2e/src/integration/app.spec.ts
+++ apps/shop-e2e/src/integration/app.spec.ts
@@ -1,13 +1,11 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, addItem, getShoppingCartStatus } from '../support/app.po';
 
 describe('shop', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
+  it('should display items', () => {
     getGreeting().contains('Welcome to shop!');
+    addItem('blanket');
+    getShoppingCartStatus().contains('1 item');
   });
 });
--- apps/shop-e2e/src/support/app.po.ts
+++ apps/shop-e2e/src/support/app.po.ts
@@ -1,1 +1,5 @@
 export const getGreeting = () => cy.get('h1');
+
+export const addItem = (code: string) => cy.get(`button[item-code = "${code}"]`)
+
+export const getShoppingCartStatus = () => cy.get(`div.cart`);
