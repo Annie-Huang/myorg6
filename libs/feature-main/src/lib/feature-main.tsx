743cf8e2c032015d995afea5b54ef82222414a71
Annie Huang <annie.hn@gmail.com>
2020-05-24 16:22:58 +1000

    nx generate @nrwl/react:library --name=feature-main --no-interactive

---
+++ libs/feature-main/src/lib/feature-main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './feature-main.css';
+
+/* eslint-disable-next-line */
+export interface FeatureMainProps {}
+
+export const FeatureMain = (props: FeatureMainProps) => {
+  return (
+    <div>
+      <h1>Welcome to feature-main!</h1>
+    </div>
+  );
+};
+
+export default FeatureMain;
