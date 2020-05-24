b45029e31d39ecbb7ede425ea1cb8683b28edef1
Annie Huang <annie.hn@gmail.com>
2020-05-24 16:31:14 +1000

    nx generate @nrwl/react:library --name=feature-about --no-interactive

---
+++ libs/feature-about/src/lib/feature-about.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import FeatureAbout from './feature-about';
+
+describe(' FeatureAbout', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<FeatureAbout />);
+    expect(baseElement).toBeTruthy();
+  });
+});
---
+++ libs/feature-about/src/lib/feature-about.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './feature-about.css';
+
+/* eslint-disable-next-line */
+export interface FeatureAboutProps {}
+
+export const FeatureAbout = (props: FeatureAboutProps) => {
+  return (
+    <div>
+      <h1>Welcome to feature-about!</h1>
+    </div>
+  );
+};
+
+export default FeatureAbout;
