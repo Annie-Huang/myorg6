71a6230561055a744323290f27443e500641d2e8
Annie Huang <annie.hn@gmail.com>
2020-05-24 10:52:06 +1000

    nx generate @nrwl/react:component --name=item-list --project=ui --export --no-interactive

---
+++ libs/ui/src/lib/item-list/item-list.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './item-list.css';
+
+/* eslint-disable-next-line */
+export interface ItemListProps {}
+
+export const ItemList = (props: ItemListProps) => {
+  return (
+    <div>
+      <h1>Welcome to item-list!</h1>
+    </div>
+  );
+};
+
+export default ItemList;
