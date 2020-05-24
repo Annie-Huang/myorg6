a650f21034410d8f5623aedb86760e97444dd089
Annie Huang <annie.hn@gmail.com>
2020-05-24 16:18:10 +1000

    nx generate @nrwl/react:library --name=feature- --no-interactive

---
+++ libs/feature-/src/lib/feature-.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './feature-.css';
+
+/* eslint-disable-next-line */
+export interface FeatureProps {}
+
+export const Feature = (props: FeatureProps) => {
+  return (
+    <div>
+      <h1>Welcome to feature-!</h1>
+    </div>
+  );
+};
+
+export default Feature;
